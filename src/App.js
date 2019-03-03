6837f1a2dc548298f878ec5b61d3142bc7b5edf0
namaste-code <shitanshu.19p@gmail.com>
2019-03-03 18:12:46 +0530

    changed app text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.<br/>
-            <code>Hello</code>
+            <code>Edited in feature branch</code>
           </p>
           <a
             className="App-link"
